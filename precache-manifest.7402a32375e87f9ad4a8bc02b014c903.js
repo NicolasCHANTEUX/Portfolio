cbbd64f0a5ecca48cf00d2955e4939bc5606a6e3
NicolasCHANTEUX <166408814+NicolasCHANTEUX@users.noreply.github.com>
2025-04-20 19:23:13 +0200

    Déploiement du site

---
+++ precache-manifest.7402a32375e87f9ad4a8bc02b014c903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b0697127ea8c3c460049f73b6ae469",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "afea0418820cf7fc064b",
+    "url": "/Portfolio/static/css/main.08865457.chunk.css"
+  },
+  {
+    "revision": "5a49e90eabab49345210",
+    "url": "/Portfolio/static/js/2.ccc2bbb4.chunk.js"
+  },
+  {
+    "revision": "99562942502e41af72fc73d84afe4332",
+    "url": "/Portfolio/static/js/2.ccc2bbb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afea0418820cf7fc064b",
+    "url": "/Portfolio/static/js/main.a31b0cb2.chunk.js"
+  },
+  {
+    "revision": "a6c51b461cb5497a2d26",
+    "url": "/Portfolio/static/js/runtime-main.d35d93f4.js"
+  }
+]);
